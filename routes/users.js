73c6830730501a28bf29b695309098eb47077b63
Anubis Lockward <anubis.lockward@gmail.com>
2025-01-02 15:45:59 -0400

    Fixed positioning and styling

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
+router.get('/access', (req, res) => {
+  res.render('users/access');
 });
 
 router.post(
   '/register',
   catchAsync(async (req, res) => {
     try {
+      console.log('Hitting register route');
       const { user } = req.body;
       const { email, username, password } = user;
 
@@ -22,9 +24,23 @@ router.post(
       res.redirect('/campgrounds');
     } catch (e) {
       req.flash('error', e.message);
-      res.redirect('/register');
+      res.redirect('/access');
     }
   })
+);
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/access',
+  }),
+  (req, res) => {
+    console.log(req.body);
+    console.log('Hitting login route');
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+  }
 );
 
 module.exports = router;
