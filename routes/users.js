702f179d2b98d32fe4ed8be9eaf39de80314e55d
Anubis Lockward <anubis.lockward@gmail.com>
2025-01-02 20:36:54 -0400

    Implement basic routing and authentication

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const { storeReturnTo } = require('../middlewares');
 const passport = require('passport');
 
 router.get('/access', (req, res) => {
@@ -10,17 +11,22 @@ router.get('/access', (req, res) => {
 
 router.post(
   '/register',
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { user } = req.body;
       const { email, username, password } = user;
 
-      const newUser = await new User({ email, username });
+      const newUser = new User({ email, username });
 
       const registeredUser = await User.register(newUser, password);
 
-      req.flash('success', 'Welcome to Yelp Camp!');
-      res.redirect('/campgrounds');
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+      });
     } catch (e) {
       req.flash('error', e.message);
       res.redirect('/access');
@@ -30,13 +36,18 @@ router.post(
 
 router.post(
   '/login',
+  storeReturnTo,
   passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/access',
   }),
   (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+
+    delete res.locals.returnTo;
+
+    res.redirect(redirectUrl);
   }
 );
 
